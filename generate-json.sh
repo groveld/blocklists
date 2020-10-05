39c79820ab76cf8aafdfe45d2d3609213429c51c
Martin Groeneveld <martin@groveld.com>
2020-10-05 10:45:34 +0200

    fixed invalid lists.json generation

--- generate-json.sh
+++ generate-json.sh
@@ -2,13 +2,12 @@
 
 [ ! -d ./lists ] && { echo "Cannot generate lists.json: lists folder not found."; exit 1; }
 
-printf "{" > ./lists/lists.json
+folderjson=()
 
 for folder in ./lists/*/; do
   folder=${folder%*/}
   foldername=${folder##*/}
-
-  printf "\"${foldername}\":{" >> ./lists/lists.json
+  filejson=()
 
   for file in ${folder}/*; do
     filename=${file##*/}
@@ -17,14 +16,13 @@ for folder in ./lists/*/; do
     filesize=$(stat -c '%s' ${file} | numfmt --to iec)
     filedate=$(head -5 ${file} | sed -n 's/^.*Updated: //p')
     filehash=$(sha1sum ${file} | cut -d' ' -f1)
-    printf "\"${filename}\":{\"file\":\"${fileurl}\",\"entries\":\"${entries}\",\"size\":\"${filesize}\",\"updated\":\"${filedate}\",\"hash\":\"${filehash}\"}," >> ./lists/lists.json
+    filejson+=("\"${filename}\":{\"file\":\"${fileurl}\",\"entries\":\"${entries}\",\"size\":\"${filesize}\",\"updated\":\"${filedate}\",\"hash\":\"${filehash}\"}")
   done
 
-  printf "}," >> ./lists/lists.json
-
+  folderjson+=("\"${foldername}\":{$(IFS=,; echo "${filejson[*]}")}")
 done
 
-printf "}" >> ./lists/lists.json
+printf "{$(IFS=,; echo "${folderjson[*]}")}" > ./lists/lists.json
 
 echo "Finished generating lists.json."
 exit 0
