bca424779b0e8dbde9994a8c9e66182f5626fc31
Martin Groeneveld <groveld@pm.me>
2021-03-14 14:59:04 +0100

    Update generate-json.sh

--- generate-json.sh
+++ generate-json.sh
@@ -15,7 +15,7 @@ for folder in ./lists/*/; do
     entries=$(head -5 ${file} | sed -n 's/^.*Entries: //p')
     filesize=$(stat -c '%s' ${file} | numfmt --to iec)
     filedate=$(head -5 ${file} | sed -n 's/^.*Updated: //p')
-    filehash=$(sha1sum ${file} | cut -d' ' -f1)
+    filehash=$(sha256sum ${file} | head -c 64)
     filejson+=("\"${filename}\":{\"file\":\"${fileurl}\",\"entries\":\"${entries}\",\"size\":\"${filesize}\",\"updated\":\"${filedate}\",\"hash\":\"${filehash}\"}")
   done
 
